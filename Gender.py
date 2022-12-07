02d42555a55192acf8433e6e1a491d112eddff62
Zapetalk <joon@brosda-altmann.de>
2022-12-07 23:20:09 +0100

    Update

--- Gender.py
+++ Gender.py
@@ -1,22 +1,37 @@
 import os
 from datetime import datetime
 
+import numpy as np
 import tensorflow as tf
+from tensorflow.python.keras import regularizers, models
+from tensorflow.python.keras.callbacks import TensorBoard
 from sklearn.metrics import accuracy_score
+from tensorflow.python.keras import layers
 
-img_height = 100
-img_width = 80
+img_height = 250
+img_width = 250
 batch_size = 1
+kernel_s=(1,1)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Input((100, 80, 1)),
-    tf.keras.layers.Conv2D(16, 3, padding='same'),
-    tf.keras.layers.Conv2D(16, 3, padding='same'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(1)
-])
 
+model=models.Sequential()
+model.add(layers.Conv2D(32,input_shape=(250,250,1),
+                        kernel_regularizer=regularizers.l2(0.001),kernel_size=4,padding="VALID"))
+model.add(layers.MaxPooling2D((2,2)))
+
+model.add(layers.Conv2D(64,kernel_s))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,kernel_s,activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+model.add(layers.Conv2D(128,kernel_s,activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,kernel_s,activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(256,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
 ds_train = tf.keras.preprocessing.image_dataset_from_directory(
     'GenderDataset/Training',
     labels='inferred',
@@ -37,18 +52,16 @@ ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_width),  # reshapes bc different heights and width
     shuffle=True,
 )
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
-    loss=[tf.keras.losses.BinaryCrossentropy(from_logits=True)],
-    metrics=["accuracy"]
-)
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 log_dir = "logs/Gender/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-model.fit(ds_train, epochs=10, verbose=2, callbacks=[tensorboard_callback])
-model.save('Gender')
+model.fit(ds_train, epochs=100, steps_per_epoch=100, validation_data=ds_validation, validation_steps=50,
+          callbacks=[tensorboard_callback])
+model.save('Gender_4.h5')
+train_label = np.hstack([y for x, y in ds_validation])
 #Get Accrusy
 y_hat = model.predict(ds_validation)
 y_hat = [0 if val < 0.5 else 1 for val in y_hat]
-print(accuracy_score(ds_validation))
+print(accuracy_score(train_label, y_hat))
 
--- Gender_Test.py
+++ Gender_Test.py
@@ -1,11 +1,12 @@
+import numpy as np
 import tensorflow as tf
 from sklearn.metrics import accuracy_score
 import sklearn as sk
 from sklearn.model_selection import train_test_split
 
 
-img_height = 100
-img_width = 80
+img_height = 250
+img_width = 250
 batch_size = 1
 
 ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
@@ -18,9 +19,10 @@ ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_width),  # reshapes bc different heights and width
     shuffle=True,
 )
-(x_validation, y_validation) =  train_test_split(ds_validation, test_size=1.0)
+train_label = np.hstack([y for x, y in ds_validation])
 
-model = tf.keras.models.load_model('Gender')
-y_hat = model.predict(x_validation)
+
+model = tf.keras.models.load_model('Gender_3.h5')
+y_hat = model.predict(ds_validation)
 y_hat = [0 if val < 0.5 else 1 for val in y_hat]
-print(accuracy_score(y_validation, y_hat))
+print(accuracy_score(train_label, y_hat))
